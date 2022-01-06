0d615b23c6a1ffca1bed4ffc6910c40a3dceb3ca
Jonathan Schmitz <jomitz96@hotmail.com>
2022-01-07 00:07:58 +0100

    Add Ramen God Site

--- src/views/ViewWrapper.tsx
+++ src/views/ViewWrapper.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {Footer} from "../components/Footer";
 import "./css/ViewWrapper.css"
-import {BrowserRouter as Router, Route, Routes, useLocation} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {MainView} from "./MainView";
 import {RamenGodView} from "./RamenGodView";
-import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 export function ViewWrapper() {
     return <div>
